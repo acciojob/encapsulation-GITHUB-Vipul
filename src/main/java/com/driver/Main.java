2f3fd494be76d0f4da102d83d3f4f768850f7a28
unknown <00vipul7@gmail.com>
2023-08-09 16:25:05 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly r=new RWOnly();
+        r.setter("aakash");
+        System.out.print(r.getter());
+    }
   
 }
