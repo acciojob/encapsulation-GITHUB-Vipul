005d9243d43b27fc66685f3ab2e67a2924eed0e3
unknown <00vipul7@gmail.com>
2023-08-09 16:51:26 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args)
     {
         RWOnly r=new RWOnly();
+        // name variable has private access modifier so direct access not possible
+
+        //setting value through setter function in RWOnly
         r.setter("aakash");
+        // printing value through getter function in RWOnly
         System.out.println(r.getter());
     }
   
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,14 +1,17 @@
 package com.driver;
 
 public class RWOnly {
+    // private access modifier
     private String name;
+    // setter function for security
     public void setter(String name)
     {
         this.name=name;
     }
+    // getter function for readOnly
     public String getter()
     {
-        return name;
+        return this.name;
     }
     
 }
